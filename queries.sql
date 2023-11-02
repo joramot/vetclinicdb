ba44e5cec7be79589d5831834e5fb1ba498d8be8
Jose RM <mojica.raul@gmail.com>
2023-11-01 22:14:45 -0600

    Queries and update animals table

--- queries.sql
+++ queries.sql
@@ -26,4 +26,62 @@ SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE NOT name = 'Gabumon';
 
 /* Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg) */
-SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3; 
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+
+
+ALTER TABLE animals ADD species varchar(100);
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+
+ROLLBACK;
+
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+
+ROLLBACK;
+
+COMMIT;
+
+
+BEGIN;
+SAVEPOINT SP1;
+DELETE FROM animals WHERE date_birth > '2022-01-01';
+UPDATE animals SET weight_kg = weight_kg*-1;
+
+ROLLBACK TO SP1;
+END;
+
+BEGIN
+UPDATE animals SET weight_kg = weight_kg*-1;
+END;
+
+COMMIT;
+
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(*)
+FROM animals
+WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg)
+FROM animals;
+
+SELECT neutered, MAX(escape_attempts)
+FROM animals GROUP BY neutered;
+
+SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight
+FROM animals
+GROUP BY species;
+
+SELECT species, AVG(escape_attempts) AS avg_escape_attempts
+FROM animals
+WHERE date_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
