958608a76f5e9c98f300085fef8b0e85a282a5bc
Jose RM <mojica.raul@gmail.com>
2023-11-03 10:25:58 -0600

    Create, update and queries in the vet_clinic db

--- data.sql
+++ data.sql
@@ -18,3 +18,29 @@ VALUES ('Charmander', '2020-02-08', 0, false, 11.00),
 ('Boarmon', '2005-06-07', 7, true, 20.4),
 ('Blossom', '1998-10-13', 3, true, 17.0),
 ('Ditto', '2022-05-14', 4, true, 22.0);
+
+INSERT INTO owners (full_name,age) 
+VALUES ('Sam Smith', 34), 
+('Jennifer Orwell', 19), 
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+INSERT INTO species (name) 
+VALUES ('Pokemon'),
+('Digimon');
+
+UPDATE animals SET specie_ID = 1 WHERE name LIKE '%mon';
+
+UPDATE animals SET specie_ID = 2 WHERE name NOT LIKE '%mon';
+
+UPDATE animals SET owner_id = 1 WHERE name LIKE 'Agumon';
+
+UPDATE animals SET owner_id = 2 WHERE name LIKE 'Gabumon'OR name LIKE 'Pikachu';
+
+UPDATE animals SET owner_id = 3 WHERE name LIKE 'Devimon' OR name LIKE 'Plantmon';
+
+UPDATE animals SET owner_id = 4 WHERE name LIKE 'Charmander' OR name LIKE 'Squirtle' OR name LIKE 'Blossom';
+
+UPDATE animals SET owner_id = 5 WHERE name LIKE 'Angemon' OR name LIKE 'Boarmon';
